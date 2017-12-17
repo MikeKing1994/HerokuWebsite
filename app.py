bbce3de38039fc7c4a43d648b1b684dcd3d87e07
MikeKing1994 <michaelj.king@btinternet.com>
2017-12-17 11:11:14 +0000

    1

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ def main():
         return render_template('indexLoggedIn.html', username = escape(session['_email']))
     else:
         print ('You are not logged in')
-        return render_template('indexLoggedOut.html')
+        return render_template('signin.html')
 
 	
 @app.route('/showSignUp')
